3c55d880c8dd13e9af22dfbec1310cac7cf711a4
chops318 <chops318@gmail.com>
2016-07-13 10:16:38 -0700

    added server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+function onRequest(req, res) {
+  console.log('Request received');
+  res.writeHead(200, { 'Content-Type': 'text/plain'});
+  res.write('Hello world');
+  res.end()
+}
+
+http.createServer(onRequest).listen(8888);
+
+console.log('Server has started')
