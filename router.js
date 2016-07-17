95db1023534a446d65d23a449da0fd6aad139f99
chops318 <chops318@gmail.com>
2016-07-17 00:44:10 -0700

    Added index and router

---
+++ router.js
@@ -0,0 +1,5 @@
+function route(pathname) {
+  console.log('About to route a req for ' + pathname);
+}
+
+exports.route = route;
--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const http = require('http');
+const url  = require('url');
 
-function onRequest(req, res) {
-  console.log('Request received');
-  res.writeHead(200, { 'Content-Type': 'text/plain'});
-  res.write('Hello world');
-  res.end()
-}
+function start(route) {
+  function onRequest(req, res) {
+    var pathname = url.parse(req.url).pathname;
+    console.log('Req for ' + pathname + ' received');
 
-http.createServer(onRequest).listen(8888);
+    route(pathname);
 
-console.log('Server has started')
+    res.writeHead(200, { 'Content-Type': 'text/plain'});
+    res.write('Hello world');
+    res.end()
+  };
+  http.createServer(onRequest).listen(8888);
+  console.log('Server has started');
+};
+
+exports.start = start;
