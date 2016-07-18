b839805d6f1f821fdd612b5f79e04e1fe30f5851
chops318 <chops318@gmail.com>
2016-07-17 17:46:44 -0700

    Added upload and show modules

--- router.js
+++ router.js
@@ -1,7 +1,7 @@
-function route(handle, pathname, response, postData) {
+function route(handle, pathname, response, request) {
   console.log('About to route a req for ' + pathname);
   if (typeof handle[pathname] === 'function') {
-    return handle[pathname](response, postData);
+    return handle[pathname](response, request);
   } else {
     console.log('No request handler for ' + pathname);
     return '404 Not found';
--- server.js
+++ server.js
@@ -3,20 +3,11 @@ const url  = require('url');
 
 function start(route, handle) {
   function onRequest(request, response) {
-    var postData = '';
     var pathname = url.parse(request.url).pathname;
     console.log('Req for ' + pathname + ' received');
-    request.setEncoding('utf8');
+    route(handle, pathname, response, request)
+  }
 
-    request.addListener('data', function(postDataChunk) {
-      postData += postDataChunk;
-      console.log('Request POST data chunk' + postDataChunk + ' .');
-    })
-
-    request.addListener('end', function() {
-      route(handle, pathname, response, postData);
-    })
-  };
   http.createServer(onRequest).listen(8888);
   console.log('Server has started');
 };
